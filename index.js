32132a175cb4a4fc0efcabb3ef5868422538849a
srb3bq46 <49237401+srb3bq46@users.noreply.github.com>
2026-05-18 22:13:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const MODULE_NAME = 'auto-d100';
+import { extension_prompt_types, extension_prompt_roles } from '../../../../script.js';
+export { MODULE_NAME };
+
+const MODULE_NAME = 'random-outcome';
 
 function rollD100() {
     return Math.floor(Math.random() * 100) + 1;
@@ -14,21 +17,26 @@ jQuery(async () => {
         saveMetadata,
     } = context;
 
-    eventSource.on(event_types.GENERATION_STARTED, async () => {
+    eventSource.on(event_types.MESSAGE_SENT, async () => {
+        console.log("triggered");
+        if (!context.chat[context.chat.length - 1].is_user){
+            console.log("not rolling - not a user message");
+            return;
+        }
+        
         const roll = rollD100();
-
-        console.log(`[${MODULE_NAME}] Rolled ${roll}`);
-
-        // Store for macros / persistence
-        chatMetadata.auto_d100 = roll;
-
+        const msg = `[SYSTEM: Current d100 roll is ${roll}. Use it for uncertainty resolution.]`
+        context.setExtensionPrompt("random-outcome-roll", msg, extension_prompt_types.IN_CHAT, 0, extension_prompt_roles.USER, false)  // k v pos depth scan role filter
+        console.log(msg);
         await saveMetadata();
-
-        // Inject into prompt
-        context.setExtensionPrompt(
-            MODULE_NAME,
-            `[SYSTEM: Current d100 roll is ${roll}. Use it for uncertainty resolution.]`,
-            'system'
-        );
     });
 });
+
+
+        // remove previous random outcome messages
+        // for (let i = context.chat.length - 1; i >= 0; i--) {
+            // console.log(i)
+            // if (context.chat[i].extra.is_random_outcome)
+                // context.deleteMessage(i)
+        // }
+        // context.sendSystemMessage('generic', msg, { is_random_outcome: true })
