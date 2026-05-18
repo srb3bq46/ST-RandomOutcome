4b3d3c4e911e5322368a42b4bf33c7aa685fc0d5
srb3bq46 <49237401+srb3bq46@users.noreply.github.com>
2026-05-18 22:17:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ jQuery(async () => {
         const msg = `[SYSTEM: Current d100 roll is ${roll}. Use it for uncertainty resolution.]`
         context.setExtensionPrompt("random-outcome-roll", msg, extension_prompt_types.IN_CHAT, 0, extension_prompt_roles.USER, false)  // k v pos depth scan role filter
         console.log(msg);
+        console.log(extension_prompt_roles.USER)
         await saveMetadata();
     });
 });
